28ac166be30389268cf83ee7d171b8add2387f48
Evan Zhu <evan.zhu2013@gmail.com>
2017-01-04 23:43:59 +0800

    Fix bugs

--- PM/PM.R
+++ PM/PM.R
@@ -15,10 +15,12 @@ library(readr)
 
 # To prevent read data again
 
+datasets <- data.frame()
+
 scraping <- function(address='北京', startyear= 2014, endyear = 2016, startmonth = 1, endmonth = 12){
         for (j in startyear : endyear){
                 for (i in startmonth : endmonth){
-                        url <- paste0('http://www.aqistudy.cn/historydata/daydata.php?city=',address,'&month=',j,'-',str_pad(i,2,side = 'left',pad='0'))
+                        url <- paste0('http://www.aqistudy.cn/historydata/daydata.php?city=',address,'&month=',j,'-',str_pad(i,2, side = 'left',pad='0'))
                         data <- url %>% read_html() %>% html_nodes('table') %>% .[[1]] %>% html_table() %>% as.data.frame()
                         datasets <- rbind(datasets, data)
                         Sys.sleep(5)
